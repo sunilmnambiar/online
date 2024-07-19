f52c0a68828bb93e3a427f8fd7d52980eafd96de
Gökay Şatır <gokaysatir@collabora.com>
2024-07-19 15:05:16 +0300

    Send the element instead of its outerhtml.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I0dad0958fe4708b601920aa4b8e2990b13da189b

--- browser/src/control/Control.UserList.ts
+++ browser/src/control/Control.UserList.ts
@@ -595,5 +595,5 @@ L.control.createUserListWidget = function () {
 	// TODO: this is not interactive
 	const userlistElement = L.DomUtil.create('div');
 	app.map.userList.renderHeaderAvatarPopover(userlistElement);
-	return userlistElement.outerHTML;
+	return userlistElement;
 };
